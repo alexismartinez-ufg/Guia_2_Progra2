b9a01b9a69769fb66d22d037cc8bb01a01b3b4f5
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-03-02 06:52:47 -0600

    Agregar archivos de proyecto.

---
+++ Ejemplo_1/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Ejemplo_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbNumerosPunto = new System.Windows.Forms.TextBox();
+            this.txbLetras = new System.Windows.Forms.TextBox();
+            this.txbEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(76, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Números y punto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(76, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Letras";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(76, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // txbNumerosPunto
+            // 
+            this.txbNumerosPunto.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNumerosPunto.Location = new System.Drawing.Point(260, 71);
+            this.txbNumerosPunto.Name = "txbNumerosPunto";
+            this.txbNumerosPunto.Size = new System.Drawing.Size(268, 27);
+            this.txbNumerosPunto.TabIndex = 3;
+            this.txbNumerosPunto.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbNumerosPunto_KeyPress);
+            // 
+            // txbLetras
+            // 
+            this.txbLetras.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbLetras.Location = new System.Drawing.Point(260, 148);
+            this.txbLetras.Name = "txbLetras";
+            this.txbLetras.Size = new System.Drawing.Size(268, 27);
+            this.txbLetras.TabIndex = 4;
+            this.txbLetras.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbLetras_KeyPress);
+            // 
+            // txbEmail
+            // 
+            this.txbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEmail.Location = new System.Drawing.Point(260, 225);
+            this.txbEmail.Name = "txbEmail";
+            this.txbEmail.Size = new System.Drawing.Size(268, 27);
+            this.txbEmail.TabIndex = 5;
+            this.txbEmail.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbEmail_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(628, 294);
+            this.Controls.Add(this.txbEmail);
+            this.Controls.Add(this.txbLetras);
+            this.Controls.Add(this.txbNumerosPunto);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txbNumerosPunto;
+        private System.Windows.Forms.TextBox txbLetras;
+        private System.Windows.Forms.TextBox txbEmail;
+    }
+}
+
---
+++ Ejemplo_1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace Ejemplo_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txbNumerosPunto_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //condicion para solo números
+            if (char.IsDigit(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            //para tecla backspace
+            else if (char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            /*verifica que pueda ingresar punto y también que solo pueda
+           ingresar un punto*/
+            else if ((e.KeyChar == '.') && (!txbNumerosPunto.Text.Contains(".")))
+            {
+                e.Handled = false;
+            }
+            //si no se cumple nada de lo anterior entonces que no lo deje pasar
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Solo se admiten datos numéricos", "validación de números",
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+        private void txbLetras_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //condicion para solo números
+            if (char.IsLetter(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            //para backspace
+            else if (char.IsControl(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            //para que admita tecla de espacio
+            else if (char.IsSeparator(e.KeyChar))
+            {
+                e.Handled = false;
+            }
+            //si no cumple nada de lo anterior que no lo deje pasar
+            else
+            {
+                e.Handled = true;
+                MessageBox.Show("Solo se admiten letras", "validación de texto",
+               MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
+
+        private void txbEmail_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (ValidateEmail(txbEmail.Text))
+            {
+                //si es correcto no debe hacer nada
+            }
+            else
+            {/*sino es correcto que envíe este mensaje y se posicione para
+verificar recuerde que se activará la validación al dar click en otro textbox o
+simplemente dejar o salir de esa casilla de email*/
+                MessageBox.Show("Dirección de correo no válida");
+                txbEmail.SelectAll(); //selecciona todo lo de la casilla
+                txbEmail.Focus(); //se posiciona ahí de nuevo
+            }
+
+        }
+
+        private static bool ValidateEmail(string email)
+        {
+            //cadena o expresion regular que verifica a un formato de correo electrónico
+            string expresion = "^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a - z]{ 2,4})$";
+            //verifica que el email ingresado corresponda con la expresion válida
+            if (Regex.IsMatch(email, expresion))
+            {//verifica que la direccion corresponda y que la longitud de la cadena no esté vacía
+                if (Regex.Replace(email, expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
