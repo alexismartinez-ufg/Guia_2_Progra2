4684b31d62cb3e39eabb647de35000ae7e14727a
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-03-02 07:38:17 -0600

    Ejemplo 4

---
+++ Ejemplo_4/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Ejemplo_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbCarnet = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.txbEdad = new System.Windows.Forms.TextBox();
+            this.btnCapturar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txbEdad);
+            this.groupBox1.Controls.Add(this.txbNombre);
+            this.groupBox1.Controls.Add(this.txbCarnet);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(358, 257);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos Empleado";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Carnet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Edad";
+            // 
+            // txbCarnet
+            // 
+            this.txbCarnet.Location = new System.Drawing.Point(136, 55);
+            this.txbCarnet.Name = "txbCarnet";
+            this.txbCarnet.Size = new System.Drawing.Size(163, 22);
+            this.txbCarnet.TabIndex = 3;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(136, 125);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(163, 22);
+            this.txbNombre.TabIndex = 4;
+            // 
+            // txbEdad
+            // 
+            this.txbEdad.Location = new System.Drawing.Point(136, 195);
+            this.txbEdad.Name = "txbEdad";
+            this.txbEdad.Size = new System.Drawing.Size(163, 22);
+            this.txbEdad.TabIndex = 5;
+            // 
+            // btnCapturar
+            // 
+            this.btnCapturar.Location = new System.Drawing.Point(77, 293);
+            this.btnCapturar.Name = "btnCapturar";
+            this.btnCapturar.Size = new System.Drawing.Size(100, 40);
+            this.btnCapturar.TabIndex = 1;
+            this.btnCapturar.Text = "Capturar";
+            this.btnCapturar.UseVisualStyleBackColor = true;
+            this.btnCapturar.Click += new System.EventHandler(this.btnCapturar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(220, 293);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(100, 40);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 345);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnCapturar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txbEdad;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.TextBox txbCarnet;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCapturar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Ejemplo_4/Form1.cs
@@ -0,0 +1,39 @@
+﻿using Ejemplo_4.Models;
+using System;
+using System.Windows.Forms;
+
+namespace Ejemplo_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCapturar_Click(object sender, System.EventArgs e)
+        {
+            Empleado miEmpleado = new Empleado();
+            try
+            {
+                miEmpleado.Carnet = int.Parse(txbCarnet.Text);
+                miEmpleado.Nombre = txbNombre.Text;
+                miEmpleado.Edad = int.Parse(txbEdad.Text);
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+                txbCarnet.Text = "";
+                txbNombre.Text = "";
+                txbEdad.Text = "";
+                txbCarnet.Focus();
+                return;
+            }
+        }
+
+        private void btnSalir_Click(object sender, System.EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
