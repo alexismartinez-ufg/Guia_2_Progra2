00be20b8380b8ae27f0759db563104a56933edc4
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-03-02 07:13:42 -0600

    ejemplo 2

---
+++ Ejemplo_2/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Ejemplo_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txbFecha = new System.Windows.Forms.DateTimePicker();
+            this.txbApellido = new System.Windows.Forms.TextBox();
+            this.txbEdad = new System.Windows.Forms.TextBox();
+            this.txbNombre = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(169, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Validaciones";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 258);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(132, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fecha de nacimiento";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 382);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Edad";
+            // 
+            // txbFecha
+            // 
+            this.txbFecha.Location = new System.Drawing.Point(208, 255);
+            this.txbFecha.Name = "txbFecha";
+            this.txbFecha.Size = new System.Drawing.Size(288, 22);
+            this.txbFecha.TabIndex = 5;
+            // 
+            // txbApellido
+            // 
+            this.txbApellido.Location = new System.Drawing.Point(208, 194);
+            this.txbApellido.Name = "txbApellido";
+            this.txbApellido.Size = new System.Drawing.Size(288, 22);
+            this.txbApellido.TabIndex = 6;
+            // 
+            // txbEdad
+            // 
+            this.txbEdad.Enabled = false;
+            this.txbEdad.Location = new System.Drawing.Point(208, 379);
+            this.txbEdad.Name = "txbEdad";
+            this.txbEdad.Size = new System.Drawing.Size(288, 22);
+            this.txbEdad.TabIndex = 7;
+            // 
+            // txbNombre
+            // 
+            this.txbNombre.Location = new System.Drawing.Point(208, 133);
+            this.txbNombre.Name = "txbNombre";
+            this.txbNombre.Size = new System.Drawing.Size(288, 22);
+            this.txbNombre.TabIndex = 8;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(233, 443);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(100, 40);
+            this.btnGuardar.TabIndex = 9;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 514);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txbNombre);
+            this.Controls.Add(this.txbEdad);
+            this.Controls.Add(this.txbApellido);
+            this.Controls.Add(this.txbFecha);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker txbFecha;
+        private System.Windows.Forms.TextBox txbApellido;
+        private System.Windows.Forms.TextBox txbEdad;
+        private System.Windows.Forms.TextBox txbNombre;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+    }
+}
+
---
+++ Ejemplo_2/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace Ejemplo_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool validarCampos()
+        {
+            //variable que verifica si algo ha sido validado
+            bool validado = true;
+            if (txbNombre.Text == "") //vefica que no quede vacío el campo
+            {
+                validado = false; //si está vacío validado es falso
+                errorProvider1.SetError(txbNombre, "Ingresar nombre"); //por lo tanto manda a llamar a errorprovider
+            //en los parámetros de setError se identifica a quién estoy validando y el mensaje que deseo mandar
+            }
+            //verifico la casilla de apellido
+            if (txbApellido.Text == "")
+            {
+                validado = false;
+                //digo que verifico a txtapellido y si no cumple mando ese mensaje
+                errorProvider1.SetError(txbApellido, "Ingrese apellido");
+            }
+            return validado;
+        }
+
+        private void BorrarMesaje()
+        {
+            //borra los mensajes para que no se muestren y pueda limpiar
+            errorProvider1.SetError(txbNombre, "");
+            errorProvider1.SetError(txbApellido, "");
+        }
+
+        private void btnGuardar_Click(object sender, System.EventArgs e)
+        {
+            //limpia cualquier mensaje de error de alguna corrida previa
+            BorrarMesaje();
+            //llamamos al método para validar campos, el de nombre y apellido
+            if (validarCampos())
+            {
+                MessageBox.Show("Los datos se ingresaron correctamente");
+            }
+            //verificamos la fecha de nacimiento que nos den
+            //DateTimePicker se llama dtpFechaNacimiento
+            DateTime fechaNacimiento = txbFecha.Value;
+            //verificamos la fecha del sistema (solo calculamos con los años
+            int anios = System.DateTime.Now.Year - fechaNacimiento.Year;
+            /*verificamos aparte del año si ya pasamos la fecha de nacimiento de este año o nos
+           faltan días*/
+            if (System.DateTime.Now.Subtract(fechaNacimiento.AddYears(anios)).TotalDays < 0)
+                //si nos faltan días para cumplir años al cálculo le resta uno
+                txbEdad.Text = Convert.ToString(anios - 1);
+            else
+                //si ya pasó nuestra fecha de nacimiento manda el valor correspondiente
+                txbEdad.Text = Convert.ToString(anios);
+
+        }
+    }
+}
