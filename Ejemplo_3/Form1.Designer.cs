5fe31e61624c054a2a83dedfa31e3142a33e05e5
ia.alexismartinez <ia.alexismartinez@ufg.edu.sv>
2024-03-02 07:25:37 -0600

    ejemplo 3

---
+++ Ejemplo_3/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Ejemplo_3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnRecargar = new System.Windows.Forms.Button();
+            this.txbDividendo = new System.Windows.Forms.TextBox();
+            this.txbDivisor = new System.Windows.Forms.TextBox();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(356, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "División de números";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label2.Location = new System.Drawing.Point(83, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dividendo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label3.Location = new System.Drawing.Point(83, 229);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Divisor";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.label4.Location = new System.Drawing.Point(83, 305);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Resultado";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(439, 175);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(100, 40);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Carlcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnRecargar
+            // 
+            this.btnRecargar.Location = new System.Drawing.Point(439, 262);
+            this.btnRecargar.Name = "btnRecargar";
+            this.btnRecargar.Size = new System.Drawing.Size(100, 40);
+            this.btnRecargar.TabIndex = 5;
+            this.btnRecargar.Text = "Recargar";
+            this.btnRecargar.UseVisualStyleBackColor = true;
+            this.btnRecargar.Click += new System.EventHandler(this.btnRecargar_Click);
+            // 
+            // txbDividendo
+            // 
+            this.txbDividendo.Location = new System.Drawing.Point(203, 150);
+            this.txbDividendo.Name = "txbDividendo";
+            this.txbDividendo.Size = new System.Drawing.Size(168, 22);
+            this.txbDividendo.TabIndex = 6;
+            // 
+            // txbDivisor
+            // 
+            this.txbDivisor.Location = new System.Drawing.Point(203, 226);
+            this.txbDivisor.Name = "txbDivisor";
+            this.txbDivisor.Size = new System.Drawing.Size(168, 22);
+            this.txbDivisor.TabIndex = 7;
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.Enabled = false;
+            this.txbResultado.Location = new System.Drawing.Point(203, 302);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.Size = new System.Drawing.Size(168, 22);
+            this.txbResultado.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 369);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.txbDivisor);
+            this.Controls.Add(this.txbDividendo);
+            this.Controls.Add(this.btnRecargar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnRecargar;
+        private System.Windows.Forms.TextBox txbDividendo;
+        private System.Windows.Forms.TextBox txbDivisor;
+        private System.Windows.Forms.TextBox txbResultado;
+    }
+}
+
---
+++ Ejemplo_3/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ejemplo_3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //tratará de realizar la acción solicitada
+            try
+            {
+                float numera = float.Parse(txbDividendo.Text);
+                float denomina = float.Parse(txbDivisor.Text);
+                float resultado = numera / denomina;
+                txbResultado.Text = Convert.ToString(resultado);
+            }
+            //si no pudiera hacerlo entonces verificará cual es el error y nos los mostrará
+            catch (Exception error)
+            {
+                MessageBox.Show("El problema es: " + error.Message);
+            }
+        }
+
+        private void btnRecargar_Click(object sender, EventArgs e)
+        {
+            txbDividendo.Clear();
+            txbDivisor.Clear();
+            txbResultado.Focus();
+        }
+    }
+}
